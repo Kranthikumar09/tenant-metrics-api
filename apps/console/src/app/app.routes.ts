16be75822325135ea514d6e4e2994a6ea701bc62
kranthi <134741860+Kranthikumar09@users.noreply.github.com>
2026-08-23 21:29:53 -0500

    feat: show account score history

--- apps/console/src/app/app.routes.ts
+++ apps/console/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { OnboardingPage } from './onboarding/onboarding';
+import { RiskHistoryPage } from './risk/risk-history';
 import { RiskPage } from './risk/risk';
 
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', redirectTo: 'onboarding' },
 	{ path: 'onboarding', component: OnboardingPage },
+	{ path: 'risk/:accountExternalId/history', component: RiskHistoryPage },
 	{ path: 'risk', component: RiskPage },
 ];
